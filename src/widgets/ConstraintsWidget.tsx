522df81e8ee3d95dc5c2ae3e3dce6f85f7dab0dc
scucuiet <40487014+SilviuMihnea@users.noreply.github.com>
2021-09-26 00:12:43 +0300

    Fix UI bug

--- src/widgets/ConstraintsWidget.tsx
+++ src/widgets/ConstraintsWidget.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Constraint from "../types/Constraint";
+import classes from "./classes";
+import "./constraints-widget.css";
 
-interface ConstraintsProps {
+export interface ConstraintsProps {
   constraints: Constraint[][];
-  extraClasses: {
+  extraClasses?: {
     container: string;
     group: string;
     constraint: string;
@@ -12,18 +14,20 @@ interface ConstraintsProps {
 
 const ConstraintsWidget: React.FC<ConstraintsProps> = ({
   constraints,
-  extraClasses,
+  extraClasses
 }) => {
   return (
-    <div className={extraClasses.container}>
+    <div className={extraClasses?.container}>
       {constraints.map((constraintGroup, i) => (
-        <div key={`constraint-group-${i}`} className={extraClasses.group}>
+        <div key={`constraint-group-${i}`} className={extraClasses?.group}>
           {constraintGroup.map((constraint, j) => {
-            const { color, count } = constraint;
+            const { color, count, completed } = constraint;
             return (
               <div
                 key={`constraint-${i}-${j}`}
-                className={`${extraClasses.constraint} ${color}`}
+                className={classes(extraClasses?.constraint, color, {
+                  completed
+                })}
               >
                 {count}
               </div>
